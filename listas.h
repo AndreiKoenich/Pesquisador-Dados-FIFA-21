456499c3957241e7cd0ea39318c23fcaa64aecce
Andrei <andrei.koenich@gmail.com>
2024-02-10 13:04:32 -0300

    Delete listas.h

--- listas.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LISTAS_H_
-#define _LISTAS_H_
-
-PonteiroPlayer* insereInicioPlayer (PonteiroPlayer *ptLista, Player dados); /* Funcao para inserir nodos no inicio de uma lista de jogadores. */
-PonteiroUser* insereInicioUser (PonteiroUser *ptLista, User dados); /* Funcao para inserir nodos no inicio de uma lista de usuarios. */
-PonteiroTag* insereInicioTag (PonteiroTag *ptLista, Info dados); /* Funcao para inserir nodos no inicio de uma lista de tags. */
-
-#endif
